f636361e946236b34e1d5d41f1a98484c69ef1bf
Nish Chal <nishchal.370@gmail.com>
2021-09-24 15:38:59 +0545

    assignment-3 completed

--- src/main/java/question4/PeopleName.java
+++ src/main/java/question4/PeopleName.java
@@ -2,8 +2,8 @@ package question4;
 
 import java.util.List;
 import java.util.Arrays;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.function.Predicate;
 
 public class PeopleName {
 
